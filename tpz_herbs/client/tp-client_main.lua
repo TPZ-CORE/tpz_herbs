d790a5e1ba664a79267611a676c0c28a5fb5872d
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2024-02-07 08:14:48 +0200

    Update tp-client_main.lua

--- tpz_herbs/client/tp-client_main.lua
+++ tpz_herbs/client/tp-client_main.lua
@@ -10,9 +10,14 @@ while true do
 	        if event == joaat("EVENT_LOOT_COMPLETE") then   -- if eventAtIndex == GetHashKey("EVENT_LOOT_COMPLETE")
 
 		    local view = exports["tpz_herbs"]:DataViewNativeGetEventData(0, index, 3)
-                     
+                    
+                    local looterId.      = view['0']
                     local lootedEntityId = view['1']
                     local isLootSuccess  = view['2']
+
+                    if isLootSuccess and PlayerPedId() == looterId then 
+                       local model = GetEntityModel(lootedEntityId)
+                    end
                 end
 	     end
          end
