4bdb096d5f608e212fe8c8b56c8de390427087d5
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2024-02-07 08:02:38 +0200

    Update tp-client_main.lua

--- tpz_herbs/client/tp-client_main.lua
+++ tpz_herbs/client/tp-client_main.lua
@@ -1,14 +1,13 @@
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
+while true do
+        Citizen.Wait(0)
 
-		local size = GetNumberOfEvents(0)   -- get number of events for EVENT GROUP 0 (SCRIPT_EVENT_QUEUE_AI). Check table below.
-		if size > 0 then
-			for i = 0, size - 1 do
+        local size = GetNumberOfEvents(0)
 
-				local eventAtIndex = GetEventAtIndex(0, i)
+        if size > 0 then
 
-				if eventAtIndex == joaat("EVENT_LOOT_COMPLETE") then   -- if eventAtIndex == GetHashKey("EVENT_LOOT_COMPLETE")
+            for index = 0, size - 1 do
+                local event = GetEventAtIndex(0, index)
+	        if event == joaat("EVENT_LOOT_COMPLETE") then   -- if eventAtIndex == GetHashKey("EVENT_LOOT_COMPLETE")
 
 					local view = exports["tpz_herbs"]:DataViewNativeGetEventData(0, index, 3)
                     
